963b36d7fe895bc35df3cd829aa029cb8b9c8b2f
asus <fadibachar.louise@std.balamand.edu.lb>
2024-11-19 22:34:06 +0300

    Create ForestTree Class

---
+++ ForestTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asus on 11/19/2024.
+//
+
+#include "ForestTree.h"
---
+++ ForestTree.h
@@ -0,0 +1,16 @@
+//
+// Created by asus on 11/19/2024.
+//
+
+#ifndef FORESTTREE_H
+#define FORESTTREE_H
+
+
+
+class ForestTree {
+
+};
+
+
+
+#endif //FORESTTREE_H
