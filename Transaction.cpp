cb6821892c0d93204bfba311ad4e3f5c05a92860
Roadan33 <roa.assaad@std.balamand.edu.lb>
2024-11-19 23:04:54 +0200

    transaction

---
+++ Transaction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AMOEBA on 11/19/2024.
+//
+
+#include "Transaction.h"
---
+++ Transaction.h
@@ -0,0 +1,8 @@
+//
+// Created by AMOEBA on 11/19/2024.
+//
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#endif //TRANSACTION_H
